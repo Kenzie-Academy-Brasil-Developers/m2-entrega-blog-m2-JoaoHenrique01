81c64a23aa0ae0d7be5cb58e36065e0e3d9863fd
Matheus <matheusdossantoscampos2@gmail.com>
2022-09-06 16:36:16 -0300

    feat: alterando trabaho

---
+++ src/scripts/modal.js
@@ -0,0 +1,17 @@
+export class Modal {
+    static showModal() {
+        const modal = document.querySelector(".modal")
+        const buttonEdit = document.querySelector("editarHistoria");
+        const buttonDelete = document.querySelector("deletePost");
+
+        buttonEdit.addEventListener("click", ()=> {
+            modal.classList.toggle("hidden")
+        })
+
+        buttonDelete.addEventListener("click",()=>{
+            modal.classList.toggle("hidden")
+        } )
+    }
+
+
+}
